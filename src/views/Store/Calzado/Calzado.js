7c1e6071d32fcc59a81f9f39923b1c2db606fa84
Einar Omar <Kiritoruizeo@gmail.com>
2023-11-19 11:55:47 -0600

    1.3.5 update tienda add filtos

--- src/views/Store/Calzado/Calzado.js
+++ src/views/Store/Calzado/Calzado.js
@@ -1,39 +1,68 @@
 import React, { useState, useEffect } from "react";
-// import { WrapperSingleRoute } from "../../../components/customs/WrapperSingleRoute";
 import { Grid, Toolbar, TextField, MenuItem, Box, Button } from "@mui/material";
 import { Bread } from "../../../components/customs/Bread";
 import { Form } from "semantic-ui-react";
 import HomeIcon from "@mui/icons-material/Home";
 import StoreIcon from "@mui/icons-material/Store";
-import RollerSkatingOutlinedIcon from "@mui/icons-material/RollerSkatingOutlined";
 import SearchIcon from "@mui/icons-material/Search";
 import {
   Search,
   SearchIconWrapper,
   StyledInputBase,
 } from "../elements/Elements.Tienda";
-import { colores, tallas, marcas, generos } from "./optionList";
-import GroupSkeleton from "../GroupSkeleton";
 import { app } from "../../../config/firebaseConnection";
 import MediaControlCard from '../MediaControlCard';
+import GroupSkeleton from "../GroupSkeleton";
+import { colores, tallas } from "./optionList";
 
 const Calzado = () => {
   const [selectedProject, setSelectedProject] = useState(null);
   const [open, setOpen] = useState(false);
+  const [filters, setFilters] = useState({
+    color: "",
+    talla: "",
+  });
+  const [searchTerm, setSearchTerm] = useState(""); // Nuevo estado para la cadena de búsqueda
+  const [proyectos, setProyectos] = useState([]);
 
   const handleClickOpen = (project) => {
     setSelectedProject(project);
     setOpen(true);
   };
 
-  const [color, setColor] = useState("");
-  const [talla, setTalla] = useState("");
-  const [proyectos, setProyectos] = useState([]);
+  const handleFilterChange = (field, value) => {
+    setFilters((prevFilters) => ({
+      ...prevFilters,
+      [field]: value,
+    }));
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchTerm(e.target.value);
+  };
 
   const obtenerInfo = async () => {
-    const docList = await app.firestore().collection("productos").get();
+    let query = app.firestore().collection("productos");
+
+    // Apply filters if color is selected
+    if (filters.color && filters.color !== "Todos") {
+      query = query.where("tipoCafe", "==", filters.color);
+    }
+
+    // Apply filters if size is selected
+    if (filters.talla && filters.talla !== "Todos") {
+      query = query.where("pais", "==", filters.talla);
+    }
+
+    // Apply search term if provided
+    if (searchTerm.trim() !== "") {
+      query = query.where("nombre", ">=", searchTerm.trim()).where("nombre", "<=", searchTerm.trim() + "\uf8ff");
+    }
+
+    const docList = await query.get();
+
     if (!docList.empty) {
-      const proyectosArray = docList.docs.map(doc => ({
+      const proyectosArray = docList.docs.map((doc) => ({
         id: doc.id,
         data: doc.data(),
       }));
@@ -43,8 +72,7 @@ const Calzado = () => {
 
   useEffect(() => {
     obtenerInfo();
-  }, []);
-
+  }, [filters, searchTerm]);
 
   return (
     <Box sx={{ bgcolor: "background.paper" }}>
@@ -54,19 +82,16 @@ const Calzado = () => {
             migas={[
               { miga: "INICIO", ruta: "/inicio", icono: <HomeIcon /> },
               { miga: "TIENDA", ruta: "/tienda", icono: <StoreIcon /> },
-             
             ]}
           />
         </Grid>
       </Grid>
-      {/* Contenido */}
       <Grid
         container
         rowSpacing={1}
         columnSpacing={1}
         sx={{ bgcolor: "background.paper", p: 1 }}
       >
-        {/* B U S C A D O R 1 */}
         <Grid item xs={12}>
           <Toolbar>
             <Search>
@@ -76,14 +101,11 @@ const Calzado = () => {
               <StyledInputBase
                 placeholder="Buscar…"
                 inputProps={{ "aria-label": "search" }}
-                // value={busqueda}
-                // onChange={handleChange}
               />
             </Search>
           </Toolbar>
         </Grid>
 
-        {/* B U S C A D O R 2 -- FIltros*/}
         <Grid item md={4} sm={6} xs={6}>
           <TextField
             component={Form.Input}
@@ -93,8 +115,8 @@ const Calzado = () => {
             type="text"
             name="color"
             color="secondary"
-            onChange={(e) => setColor(e.target.value)}
-            value={color || ""}
+            onChange={(e) => handleFilterChange("color", e.target.value)}
+            value={filters.color || ""}
             autoComplete="off"
           >
             {colores.map((color) => (
@@ -114,8 +136,8 @@ const Calzado = () => {
             type="text"
             color="secondary"
             name="talla"
-            onChange={(e) => setTalla(e.target.value)}
-            value={talla || ""}
+            onChange={(e) => handleFilterChange("talla", e.target.value)}
+            value={filters.talla || ""}
             autoComplete="off"
           >
             {tallas.map((talla) => (
@@ -126,40 +148,34 @@ const Calzado = () => {
           </TextField>
         </Grid>
 
-        {/* BUSCADOR */}
         <Grid item md={4} sm={12} xs={12}>
           <Box display="flex" height="100%">
             <Button fullWidth variant="contained" sx={{
-        minWidth: 200,
-        backgroundColor: "#886750",
-        ':hover': {
-          backgroundColor: "#886750", // Mismo color que el color normal
-        },
-        transition: "background-color 0.3s", // Agregar una transición suave para el cambio de color
-      }}>
-              {" "}
-              {/* onClick={handleSearch} */}
+              minWidth: 200,
+              backgroundColor: "#886750",
+              ':hover': {
+                backgroundColor: "#886750",
+              },
+              transition: "background-color 0.3s",
+            }}>
               Buscar
             </Button>
           </Box>
         </Grid>
-
       </Grid>
       <Box sx={{ p: { xs: 1, md: 3 }, flexGrow: 1 }}>
         <Grid container spacing={{ xs: 3, sm: 2, md: 2 }}>
           {proyectos.length === 0 ? (
             <GroupSkeleton />
           ) : (
-            proyectos.map((proyecto, index) => {
-              return (
-                <Grid item xs={12} sm={12} md={12} lg={6} xl={4} key={index}>
-                  <MediaControlCard
-                    proyecto={proyecto}
-                    handleClickOpen={handleClickOpen}
-                  />
-                </Grid>
-              );
-            })
+            proyectos.map((proyecto, index) => (
+              <Grid item xs={12} sm={12} md={12} lg={6} xl={4} key={index}>
+                <MediaControlCard
+                  proyecto={proyecto}
+                  handleClickOpen={handleClickOpen}
+                />
+              </Grid>
+            ))
           )}
         </Grid>
       </Box>
