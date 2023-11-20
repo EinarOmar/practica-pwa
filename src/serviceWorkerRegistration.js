ac8193d0e576ad2bcecc00b9a028d713269d98bb
Einar Omar <Kiritoruizeo@gmail.com>
2023-11-19 22:13:56 -0600

    3.3.3 update sw

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -1,36 +1,35 @@
 // serviceWorkerRegistration.js
 export function register(config) {
-    if ('serviceWorker' in navigator) {
-      window.addEventListener('load', () => {
-        const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;  // Ruta relativa a la carpeta 'public'
   
-        navigator.serviceWorker
-          .register(swUrl)
-          .then((registration) => {
-            console.log('Service Worker registrado con éxito:', registration);
-            if (config && config.onSuccess) {
-              config.onSuccess(registration);
-            }
-          })
-          .catch((error) => {
-            console.error('Error al registrar el Service Worker:', error);
-            if (config && config.onError) {
-              config.onError(error);
-            }
-          });
-      });
-    }
-  }
-  
-  export function unregister() {
-    if ('serviceWorker' in navigator) {
-      navigator.serviceWorker.ready
+      navigator.serviceWorker
+        .register(swUrl)
         .then((registration) => {
-          registration.unregister();
+          console.log('Service Worker registrado con éxito:', registration);
+          if (config && config.onSuccess) {
+            config.onSuccess(registration);
+          }
         })
         .catch((error) => {
-          console.error('Error al desregistrar el Service Worker:', error);
+          console.error('Error al registrar el Service Worker:', error);
+          if (config && config.onError) {
+            config.onError(error);
+          }
         });
-    }
+    });
   }
-  
+}
+
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready
+      .then((registration) => {
+        registration.unregister();
+      })
+      .catch((error) => {
+        console.error('Error al desregistrar el Service Worker:', error);
+      });
+  }
+}
